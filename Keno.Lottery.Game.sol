fb4f7d7c2a5bcd203c0749b6e81847437d4acd6a
Shannon Randhawa <79070530+shannonrandhawa@users.noreply.github.com>
2022-05-07 01:21:04 -0500

    Create Keno.Lottery.Game.sol

---
+++ Keno.Lottery.Game.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.5.0 <0.9.0;
+
+
+contract Lottery{
+    address payable[] public players;
+    address public manager;
+
+constructor(){
+   manager = msg.sender;
+}
+
+receive() external payable{
+    require(msg.value == 0.1 ether);
+    players.push(payable(msg.sender));
+}
+
+function getBalance() public view returns(uint){
+    require(msg.sender == manager);
+    return address(this).balance;
+}
+}
+
+
+// SPDX-License-Identifier: MIT
